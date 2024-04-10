7326cf2c48b1ba173379c713e6ed70f5136ff82e
sierracorl <scorl@umich.edu>
2024-04-10 19:08:32 -0400

    Made changes
    Made it to how michele wants it

--- cutz-mobileapp-main/src/screens/MainScreens/AppDescription/AppDescription.js
+++ cutz-mobileapp-main/src/screens/MainScreens/AppDescription/AppDescription.js
@@ -52,7 +52,7 @@ const AppDescription = ({ navigation }) => {
                         label="App Description"
                         fontFamily="semiBold"
                         fontSize={18}
-                        color={"#000000"}
+                        color={"#969696"}
                     />
                 </View>
                 <Spacer height={20}/>
@@ -60,12 +60,12 @@ const AppDescription = ({ navigation }) => {
                 <CustomText
     label={"The app that gets you in line, in time.  "}
     fontFamily="bold"
-    fontSize={32}
+    fontSize={20}
     color={"#000000"}
     style={{ textAlign: "center" }}
 />
                     <CustomText
-                        label={"Cutz connect users with non-profits, for-profits, and government organizations and helps manage large events by eliminating chaos and congestion."}
+                        label={"Cutz connect users with non-profits, for-profits, and government organizations and helps manage large events by eliminating chaos and congestion.\n"}
                         fontFamily="regular"
                         fontSize={14}
                         color={"#000000"}
@@ -73,130 +73,186 @@ const AppDescription = ({ navigation }) => {
                     <CustomText
                         label={"Looking For Help?"}
                         fontFamily="semiBold"
-                        fontSize={32}
+                        fontSize={20}
                         color={"#000000"}
                     />
                     <CustomText
-                        label={"SELECT CLIENT TAB — Complete Registration."}
+                        label={"SELECT CLIENT TAB — Complete Registration.\n"}
                         fontFamily="regular"
-                        fontSize={14}
+                        fontSize={16}
                         color={"#000000"}
                     />
                     <CustomText
                         label={"1. Select an organization, an event, location, date, and time you would like to reserve. (Note: Time slots will disappear when they are filled.)"}
                         fontFamily="regular"
-                        fontSize={14}
+                        fontSize={12}
                         color={"#000000"}
                     />
                     <CustomText
-                        label={"2. Get in line when you are alerted in the app."}
+                        label={"\n2. Get in line when you are alerted in the app."}
+                        fontFamily="regular"
+                        fontSize={12}
+                        color={"#000000"}
+                    />
+                    <CustomText
+                        label={"\n3. Have the Cutz app open and screen ready to allow a volunteer to type a 4 digit event code upon entry."}
+                        fontFamily="regular"
+                        fontSize={12}
+                        color={"#000000"}
+                    />
+                    <CustomText
+                        label={"\n4. Ready to exit? Hold your screen towards the volunteer and allow them to enter a 4 digit check-out code.\n\n"}
+                        fontFamily="regular"
+                        fontSize={12}
+                        color={"#000000"}
+                    />
+                    <CustomText
+                        label={"IMPORTANT:"}
+                        fontFamily="Bold"
+                        fontSize={14}
+                        color={"#000000"}
+                    />
+                                       <CustomText
+                        label={"If you do not have reservations for an event you would like to attend, you may enter the area 15 minutes prior to end of the event. There are no guarantees without reservations.\n "}
+                        fontFamily="regular"
+                        fontSize={14}
+                        color={"#000000"}
+                    /> 
+                                 <CustomText
+                        label={"NOTIFICATIONS:"}
+                        fontFamily="Bold"
+                        fontSize={14}
+                        color={"#000000"}
+                    /> 
+              <CustomText
+                        label={"If an event is cancelled or if there are changes to an event, you will find the details in the notifications section."}
                         fontFamily="regular"
                         fontSize={14}
                         color={"#000000"}
                     />
                     <CustomText
-                        label={"3. Have the Cutz app open and screen ready to allow a volunteer to type a 4 digit event code upon entry."}
-                        fontFamily="regular"
-                        fontSize={14}
-                        color={"#000000"}
-                    />
-                    <CustomText
-                        label={"4. Ready to exit? Hold your screen towards the volunteer and allow them to enter a 4 digit check-out code."}
-                        fontFamily="regular"
-                        fontSize={14}
-                        color={"#000000"}
-                    />
-                    <CustomText
-                        label={"IMPORTANT: If you do not have reservations for an event you would like to attend, you may enter the area 15 minutes prior to end of the event. There are no guarantees without reservations."}
-                        fontFamily="regular"
-                        fontSize={14}
-                        color={"#000000"}
-                    />
-                    <CustomText
-                        label={"NOTIFICATIONS: If an event is cancelled or if there are changes to an event, you will find the details in the notifications section."}
-                        fontFamily="regular"
-                        fontSize={14}
-                        color={"#000000"}
-                    />
-                    <CustomText
-                        label={"When you make reservations and are late or a no-show, you will not be allowed in until 15 minutes prior to the end of the event. Please get in line with those waiting to enter without a reservation. If you miss your reservations 3 times, you may be eliminated from making future reservations by the Admin of the organization(s)."}
-                        fontFamily="regular"
-                        fontSize={14}
-                        color={"#000000"}
-                    />
-                    <CustomText
-                        label={"Want to Volunteer?"}
+                        label={"\nWhen you make reservations and are late or a no-show, you will not be allowed in until 15 minutes prior to the end of the event. Please get in line with those waiting to enter without a reservation. If you miss your reservations 3 times, you may be eliminated from making future reservations by the Admin of the organization(s)."}
                         fontFamily="semiBold"
-                        fontSize={16}
+                        fontSize={14}
                         color={"#000000"}
                     />
                     <CustomText
-                        label={"SELECT VOLUNTEER TAB — COMPLETE REGISTRATION."}
+                        label={"\nWant to Volunteer?"}
+                        fontFamily="semiBold"
+                        fontSize={20}
+                        color={"#000000"}
+                    />
+                    <CustomText
+                        label={"SELECT VOLUNTEER TAB — COMPLETE REGISTRATION.\n"}
                         fontFamily="regular"
-                        fontSize={14}
+                        fontSize={16}
                         color={"#000000"}
                     />
                     <CustomText
                         label={"1. Select an organization, an event, location, date, and time you would like to volunteer. (Note: Time slots will disappear when they are filled.)"}
                         fontFamily="regular"
-                        fontSize={14}
+                        fontSize={12}
                         color={"#000000"}
                     />
                     <CustomText
-                        label={"2. Check-in with an Admin as soon as you arrive and allow them to enter your volunteer event code (upon arrival and when your shift is complete)."}
+                        label={"\n2. Check-in with an Admin as soon as you arrive and allow them to enter your volunteer event code (upon arrival and when your shift is complete)."}
+                        fontFamily="regular"
+                        fontSize={12}
+                        color={"#000000"}
+                    />
+                                      <CustomText
+                        label={"\n\nIMPORTANT:"}
+                        fontFamily="Bold"
+                        fontSize={14}
+                        color={"#000000"}
+                    /> 
+                <CustomText
+                        label={"If you are late or a no-show for volunteering, it slows the entire event down. Remember, the Organization is relying on its Volunteers to keep things flowing and on schedule. Please be on-time and stay through your shift. All late Volunteers, must see an Admin before you begin work. Volunteers are asked to help with clean-up at the end of the event.\n\n"}
                         fontFamily="regular"
                         fontSize={14}
                         color={"#000000"}
                     />
                     <CustomText
-                        label={"IMPORTANT: If you are late or a no-show for volunteering, it slows the entire event down. Remember, the Organization is relying on its Volunteers to keep things flowing and on schedule. Please be on-time and stay through your shift. All late Volunteers, must see an Admin before you begin work. Volunteers are asked to help with clean-up at the end of the event."}
+                        label={"Developed in cooperation with UM-D."}
+                        fontFamily="Bold"
+                        fontSize={16}
+                        color={"#000000"}
+                    />
+                                      <CustomText
+                        label={"Special “thank you” to:\n"}
+                        fontFamily="regular"
+                        fontSize={16}
+                        color={"#000000"}
+                    /> 
+                <CustomText
+                        label={"Dr. Bruce Maxim, "}
                         fontFamily="regular"
                         fontSize={14}
                         color={"#000000"}
                     />
                     <CustomText
-                        label={"Developed in cooperation with UM-D. Special “thank you” to:"}
+                        label={"PhD Natty Natarajan Professor of Engineering Computer and Information Science "}
+                        fontFamily="regular"
+                        fontSize={12}
+                        color={"#000000"}
+                    />
+                    <CustomText
+                        label={"Dr. Mahmoud Abou-Nasr,"}
+                        fontFamily="regular"
+                        fontSize={14}
+                        color={"#000000"}
+                    />
+                                      <CustomText
+                        label={"PhD Professor of Engineering Computer and Information Science"}
+                        fontFamily="regular"
+                        fontSize={12}
+                        color={"#000000"}
+                    /> 
+               <CustomText
+                        label={"\nMichele Maltese — Concept / Design / UI/UX"}
                         fontFamily="regular"
                         fontSize={14}
                         color={"#000000"}
                     />
                     <CustomText
-                        label={"Dr. Bruce Maxim, PhD Natty Natarajan Professor of Engineering Computer and Information Science"}
+                        label={"Jennifer Kujawski — Concept Support / Admin Experiences\n"}
                         fontFamily="regular"
                         fontSize={14}
                         color={"#000000"}
                     />
-                    <CustomText
-                        label={"and"}
+                                      <CustomText
+                        label={"Team 2"}
+                        fontFamily="regular"
+                        fontSize={16}
+                        color={"#000000"}
+                    /> 
+               <CustomText
+                        label={"Shahd Mustafa — Project Manager"}
                         fontFamily="regular"
                         fontSize={14}
                         color={"#000000"}
                     />
-                    <CustomText
-                        label={"Dr. Mahmoud Abou-Nasr, PhD Professor of Engineering Computer and Information Science"}
+                                                     <CustomText
+                        label={"Meher Jabber — Developer"}
                         fontFamily="regular"
                         fontSize={14}
                         color={"#000000"}
-                    />
-                    <CustomText
-                        label={"Michele Maltese — Concept / Design / UI/UX"}
+                    /> 
+                                                    <CustomText
+                        label={"Chase Stuck — Developer"}
                         fontFamily="regular"
                         fontSize={14}
                         color={"#000000"}
-                    />
-                    <CustomText
-                        label={"Jennifer Kujawski — Concept Support / Admin Experiences"}
+                    /> 
+                                                   <CustomText
+                        label={"Sierra Corl — Developer"}
                         fontFamily="regular"
                         fontSize={14}
                         color={"#000000"}
-                    />
-                    <CustomText
-                        label={"Team 2 — Shahd Mustafa — Project Manager / Developer Meher Jabber — Developer Chase Stuck — Developer Sierra Corl — Developer"}
-                        fontFamily="regular"
-                        fontSize={14}
-                        color={"#000000"}
-                    />
-                </ScrollView>
+                    />  
+              
+              
+               </ScrollView>
             </PH20>
             <Loader file={loaderAnimation} loading={loading}/>
         </SafeAreaView>);
